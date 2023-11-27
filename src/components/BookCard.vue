06a5c637ab2ba3d754fb352c1155aaa405c4793e
simkaurd <kaurs61@myumanitoba.ca>
2023-11-27 17:20:10 -0600

    Added a few books and worked on pop-up

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -1,20 +1,55 @@
 <template>
+<div class="clearfix">
   <v-card
     class="ma-4"
+    hover
     width="140"
     height="200"
-    elevation="0"
+    elevation="6"
     :image="book.cover"
+    link
   >
 
   <v-dialog
         v-model="dialog"
         activator="parent"
-        width="auto"
+        width=60%
       >
         <v-card>
+          <v-card-title> {{ book.title }} </v-card-title>
           <v-card-text>
-            {{ book.title }}
+        
+          <div style="display: flex; font-size:12px; width: 100%;">
+            <div style="flex: 40%; padding-right: 16px;"> 
+                <img :src=book.cover class="custom-image">
+                <strong>{{ book.rating }}&nbsp</strong> 
+                <v-rating
+                half-increments
+                hover
+                readonly
+                :length="5"
+                :size="13"
+                :model-value=book.rating
+                active-color="warning"
+                density="auto"
+                />
+            </div>
+
+            <div style="flex: 60%; font-size: 14px;">
+              <br>
+                <strong>Author:</strong> {{ book.author }}
+              <br>
+                <strong>ISBN:</strong> {{ book.isbn }}
+              <br>
+                <strong>Edition:</strong> {{ book.edition }}
+              <br>
+                <strong>Year:</strong> {{ book.year }}
+              <br>
+                <strong>Price:</strong> {{ book.price }}
+              <br>
+                <strong>Units available:</strong> {{ book.available }}
+            </div>
+            </div>
           </v-card-text>
           <v-card-actions>
             <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
@@ -23,6 +58,7 @@
     </v-dialog>
 
   </v-card>
+  </div>
 </template>
 
 
@@ -34,3 +70,22 @@
     }),
   }
 </script>
+
+<style scoped>
+.column {
+  
+  margin: 1%; 
+  float: left;
+}
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+.custom-image {
+  width: 100%; 
+  height: 200px;
+  object-fit: contain;
+  object-position: left;
+}
+</style>
