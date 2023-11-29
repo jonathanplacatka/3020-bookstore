b1112b0c5fe8bf20e448ce55cb2cdae4ef5cf0e6
Jonathan Placatka <jplacatka@live.ca>
2023-11-28 18:09:50 -0600

    add shopping cart total and handle duplicates

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,31 +1,43 @@
 <template>
-      <v-app-bar style="background-color: MediumSeaGreen" elevation="0" class="px-auto">
+      <v-app-bar style="background-color: MediumSeaGreen" elevation="0">
           <v-app-bar-nav-icon color="white"/>
+          <v-spacer></v-spacer>
           <v-badge 
             color="red"
-            :content="items.length"
+            :content="numItems()"
             :model-value="items.length > 0"
+            class="pr-6"
           >
             <v-icon icon="mdi-cart" color="white" size="large" id="menu-activator"></v-icon>
           </v-badge>
 
       </v-app-bar>
 
-
       <v-menu
         transition="slide-y-transition"
         activator="#menu-activator"
       >
-        <v-list>
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
+        <v-card>
+          <v-col
+              align="center"
           >
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+            <v-list>
+              <v-list-item
+                v-for="(item, i) in items"
+                :key="i"
+              >
+                <v-list-item-title class="text-left">{{ item.book.title }} ({{ item.quantity }}) - <b>${{ item.book.price }}</b> </v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title class="text-left"> <b>Total: {{ this.total() }}</b></v-list-item-title>
+              </v-list-item>
+            </v-list>
 
+            <v-btn>Checkout</v-btn>
+
+          </v-col>
+        </v-card>
+      </v-menu>
 </template>
 
 <script>
@@ -35,8 +47,29 @@
     }),
     mounted() { 
       this.emitter.on("add-to-cart", book => {
-        this.items.push(book);
+        this.addItem(book)
       });
+    },
+    methods: {
+      addItem(book) {
+        let result = this.items.find(item => item.book === book) 
+        if(result) {        
+          result.quantity++
+        } else {
+          this.items.push({book: book, quantity: 1})
+        }
+      },
+      numItems() {
+        let sum = 0;
+        this.items.forEach(item => {sum += item.quantity});
+        return sum;
+      },
+      total() {
+        let sum = 0;
+        this.items.forEach(item => (sum += (item.book.price*item.quantity)))
+        return sum;
+      }
     }
   }
 </script>
+
