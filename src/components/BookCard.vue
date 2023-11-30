ab552829614afd3830f28d69d2c034caa2390e86
mespn <manuelen2002@gmail.com>
2023-11-29 23:10:40 -0600

    Added radio buttons, no functionality

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -48,6 +48,16 @@
                     <strong>Units available:</strong> {{ book.available }}
                  </div>
                 </div>
+                <div>
+
+                  <input type="radio" id="physical" value="physical" name="type-of-book"/>
+                  <label for="physical">Physical:&emsp;${{ book.price }}</label><br>
+
+                  <input type="radio" id="digital" value="digital"  name="type-of-book"/>
+                  <label for="digital">Digital:&emsp;${{(book.price *0.66).toFixed(2)}}</label><br>
+
+                </div>
+
                 <v-btn @click="addToCart()">
                   Add to Cart
                 </v-btn>
@@ -67,6 +77,7 @@
 </template>
 
 <script>
+
 export default {
   props: ['book'],
   data: () => ({
