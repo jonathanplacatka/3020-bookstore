cbd51d1e5e8d41c7077c9c2e9363933bad3dcad2
arasantos <100260167+arasantos@users.noreply.github.com>
2023-11-29 18:26:00 -0600

    added more books, filled in missing info, made pop up disappear when added to cart

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -1,54 +1,34 @@
 <template>
-<div class="clearfix">
-  <v-card
-    class="ma-4"
-    hover
-    width="140"
-    height="200"
-    elevation="0"
-    :image="book.cover"
-    link
-  >
+  <div class="clearfix">
+    <v-card class="ma-4" hover width="140" height="200" elevation="0" :image="book.cover" link>
 
-  <v-dialog
-        v-model="dialog"
-        activator="parent"
-        width=60%
-      >
+      <v-dialog v-model="dialog" activator="parent" width=60%>
         <v-card>
           <v-card-title> {{ book.title }} </v-card-title>
           <v-card-text>
-        
-          <div style="display: flex; font-size:12px; width: 100%;">
-            <div style="flex: 40%; padding-right: 16px;"> 
-                <img :src=book.cover class="custom-image">
-                <strong>{{ book.rating }}&nbsp</strong> 
-                <v-rating
-                half-increments
-                hover
-                readonly
-                :length="5"
-                :size="13"
-                :model-value=book.rating
-                active-color="warning"
-                density="auto"
-                />
-            </div>
 
-            <div style="flex: 60%; font-size: 14px;">
-              <br>
+            <div style="display: flex; font-size:12px; width: 100%;">
+              <div style="flex: 40%; padding-right: 16px;">
+                <img :src=book.cover class="custom-image">
+                <strong>{{ book.rating }}&nbsp</strong>
+                <v-rating half-increments hover readonly :length="5" :size="13" :model-value=book.rating
+                  active-color="warning" density="auto" />
+              </div>
+
+              <div style="flex: 60%; font-size: 14px;">
+                <br>
                 <strong>Author:</strong> {{ book.author }}
-              <br>
+                <br>
                 <strong>ISBN:</strong> {{ book.isbn }}
-              <br>
+                <br>
                 <strong>Edition:</strong> {{ book.edition }}
-              <br>
+                <br>
                 <strong>Year:</strong> {{ book.year }}
-              <br>
+                <br>
                 <strong>Price:</strong> {{ book.price }}
-              <br>
+                <br>
                 <strong>Units available:</strong> {{ book.available }}
-            </div>
+              </div>
             </div>
             <v-btn @click="addToCart()">
               Add to Cart
@@ -58,40 +38,43 @@
             <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
           </v-card-actions>
         </v-card>
-    </v-dialog>
+      </v-dialog>
 
-  </v-card>
+    </v-card>
   </div>
 </template>
 
 
 <script>
-  export default {
-    props: ['book'],
-    data: () => ({
-      dialog: false,
-    }),
-    methods: {
-      addToCart() {
-        this.emitter.emit('add-to-cart', this.book);
-      }
+export default {
+  props: ['book'],
+  data: () => ({
+    dialog: false,
+  }),
+  methods: {
+    addToCart() {
+      this.emitter.emit('add-to-cart', this.book);
+      this.dialog = false;
     }
   }
+}
 </script>
 
 <style scoped>
 .column {
-  
-  margin: 1%; 
+
+  margin: 1%;
   float: left;
 }
+
 .clearfix::after {
   content: "";
   clear: both;
   display: table;
 }
+
 .custom-image {
-  width: 100%; 
+  width: 100%;
   height: 200px;
   object-fit: contain;
   object-position: left;
